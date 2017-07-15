252c9de2b56689c3e1c92c0b9b639cb8fc601f3a
Jonathan Meyer <jon@gisjedi.com>
2017-07-14 21:36:17 -0400

    Updated manifests to be applied to the Dockerfiles for documentation.

--- examples/inject-manifests.sh
+++ examples/inject-manifests.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Ensure script directory is CWD
+cd "${0%/*}"
+
+# Inject the manifest for each example into SEED_MANIFEST placeholder
 for DIRECTORY in $(ls -F | grep /)
 do
-    SEED_MANIFEST=$(jq -rc . < ${DIRECTORY}/seed.manifest.json | sed 's/\"/\\\"/g')
-    sed -i "" -e "s/SEED_MANIFEST/"${SEED_MANIFEST}"/" ${DIRECTORY}/Dockerfile
+    SEED_MANIFEST=$(jq -rc . < ${DIRECTORY}/seed.manifest.json | sed 's/\"/\\\\\"/g')
+    sed -i "" -e "s^SEED_MANIFEST^${SEED_MANIFEST}^" ${DIRECTORY}/Dockerfile
 done
+
+cd -
