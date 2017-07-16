edd70cde3284abaf58d31d671c178ac489deed1b
Jonathan Meyer <jon@gisjedi.com>
2017-07-16 12:30:17 -0400

    Use sudo to manipulate files output by docker run of asciidoctor

--- spec/collect-versioned-docs.sh
+++ spec/collect-versioned-docs.sh
@@ -5,14 +5,14 @@ cd "${0%/*}"/output
 
 if [[ "${TRAVIS_TAG}x" != "x" ]]
 then
-    cp detail.html seed-${TRAVIS_TAG}.html
-    cp detail.pdf seed-${TRAVIS_TAG}.pdf;
+    sudo cp detail.html seed-${TRAVIS_TAG}.html
+    sudo cp detail.pdf seed-${TRAVIS_TAG}.pdf;
 fi
 
 # Grab all available versions to place in gh-pages
 for VERSION in $(cat ../../.versions)
 do
-    wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed-${VERSION}.html
+    sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed-${VERSION}.html
 done
 
 cd - > /dev/null
