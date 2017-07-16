2722453feec8719b61715e8dccc58d18792f29c8
Jonathan Meyer <jon@gisjedi.com>
2017-07-16 14:02:55 -0400

    Skip gh-pages data collection for release builds

--- spec/collect-versioned-docs.sh
+++ spec/collect-versioned-docs.sh
@@ -4,24 +4,27 @@
 OUTPUT_DIR="${0%/*}"/output
 pushd $OUTPUT_DIR
 
+# Collect files for GitHub Releases
 if [[ "${TRAVIS_TAG}x" != "x" ]]
 then
     sudo mkdir ${TRAVIS_TAG}
     sudo cp -R seed.* $TRAVIS_TAG/
-    sudo cp -R schema/*.json $TRAVIS_TAG/
+    sudo cp -R ../schema/*.json $TRAVIS_TAG/
 fi
 
 # Grab all available versions to place in gh-pages
-for VERSION in $(cat ../../.versions)
-do
-    cd $OUTPUT_DIR
-    sudo mkdir -p $VERSION/schema
-    cd $VERSION
-    sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.html
-    sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.pdf
-    cd $VERSION/schema
-    sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.manifest.schema.json
-    sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.metadata.schema.json
-done
-
+if [[ "${TRAVIS_TAG}x" == "x" ]]
+then
+    for VERSION in $(cat ../../.versions)
+    do
+        cd $OUTPUT_DIR
+        sudo mkdir -p $VERSION/schema
+        cd $VERSION
+        sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.html
+        sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.pdf
+        cd $VERSION/schema
+        sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.manifest.schema.json
+        sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.metadata.schema.json
+    done
+fi
 popd > /dev/null
