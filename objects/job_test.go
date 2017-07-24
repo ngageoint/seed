7394905a2523a5fdb2f2a45897b3750206609f66
Jonathan Meyer <jon@gisjedi.com>
2017-07-24 13:27:30 -0400

    Fixed build to include spec docs for all github pages publish

--- objects/job_test.go
+++
@@ -1,9 +0,0 @@
-package objects
-
-import (
-	"testing"
-)
-
-func TestJob(t *testing.T) {
-	//do nothing
-}
--- objects/seed.go
+++
@@ -1,6 +0,0 @@
-package objects
-
-type Seed_0_0_4 struct {
-	SeedVersion string
-	Job SeedJob
-}
