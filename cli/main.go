409430f75bcdacecaa2a5f9067754673c83c376d
emimaesmith <emimaesmith@gmail.com>
2017-07-24 15:38:54 -0400

    seed list CLI command implementation

--- cli/main.go
+++ cli/main.go
@@ -85,6 +85,12 @@ func main() {
 	// Parse input flags
 	DefineFlags()
 
+	// Lists all seed compliant images on (default) local machine
+	if listCmd.Parsed() {
+		DockerList()
+		os.Exit(1)
+	}
+
 	// Define the current working directory
 	curDirectory, _ := os.Getwd()
 
@@ -159,6 +165,42 @@ func main() {
 	// Run Docker image
 	if runCmd.Parsed() {
 		DockerRun(&seed)
+	}
+}
+
+//DockerList lists all seed compliant images (ending with -seed) on the local
+//	system
+func DockerList() {
+	dCmd := exec.Command("docker", "images")
+	gCmd := exec.Command("grep", "seed")
+	var dErr bytes.Buffer
+	dCmd.Stderr = &dErr
+	dOut, err := dCmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Error attaching to std output pipe. %s\n", err.Error())
+	}
+
+	dCmd.Start()
+	if string(dErr.Bytes()) != "" {
+		fmt.Fprintf(os.Stderr, "ERROR: Error reading stderr %s\n", string(dErr.Bytes()))
+	}
+
+	gCmd.Stdin = dOut
+	var gErr bytes.Buffer
+	gCmd.Stderr = &gErr
+
+	o, err := gCmd.Output()
+	fmt.Fprintf(os.Stderr, string(gErr.Bytes()))
+	if string(gErr.Bytes()) != "" {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", string(gErr.Bytes()))
+	}
+	if err != nil && err.Error() != "exit status 1" {
+		fmt.Fprintf(os.Stderr, "ERROR: Error executing seed list: %s\n", err.Error())
+	}
+	if string(o) == "" {
+		fmt.Fprintf(os.Stderr, "No Seed Images found!\n")
+	} else {
+		fmt.Fprintf(os.Stderr, "%s\n", string(o))
 	}
 }
 
@@ -450,8 +492,7 @@ func DefineFlags() {
 		fmt.Fprintf(os.Stderr, "%q is not yet implemented\n\n", os.Args[1])
 		PrintSearchUsage()
 	case constants.ListCommand:
-		fmt.Fprintf(os.Stderr, "%q is not yet implemented\n\n", os.Args[1])
-		PrintListUsage()
+		listCmd.Parse(os.Args[2:])
 	case constants.PublishCommand:
 		fmt.Fprintf(os.Stderr, "%q is not yet implemented\n\n", os.Args[1])
 		PrintPublishUsage()
