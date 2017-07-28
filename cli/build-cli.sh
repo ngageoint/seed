19db69a49b6a4a429963679ca689038672579817
Jonathan Meyer <jon@gisjedi.com>
2017-07-27 21:04:24 -0400

    Enabling static linking of CLI binaries

--- cli/build-cli.sh
+++ cli/build-cli.sh
@@ -18,11 +18,11 @@ fi
 vendor/go-bindata -pkg constants -o constants/jsonschema.go ../spec/schema/seed.manifest.schema.json
 echo Building cross platform Seed CLI.
 echo Building for Linux...
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o output/seed-linux-amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags "-X main.version=$VERSION -extldflags=\"-static\"" -o output/seed-linux-amd64
 echo Building for OSX...
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o output/seed-darwin-amd64
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -ldflags "-X main.version=$VERSION -extldflags=\"-static\"" -o output/seed-darwin-amd64
 echo Building for Windows...
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o output/seed-windows-amd64
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags "-X main.version=$VERSION -extldflags=\"-static\"" -o output/seed-windows-amd64
 echo CLI build complete
 
 popd >/dev/null
