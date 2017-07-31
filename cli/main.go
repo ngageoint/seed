942c710241f29155fb0b4f787ac130b688e74409
emimaesmith <emimaesmith@gmail.com>
2017-07-31 13:44:26 -0400

    #38 docker access validation; #44 checks docker version for --label build argument
    #38 Runs a docker info command to check if seed needs to be run as
    sudo. Informs user if so.
    #44 Validates docker version during an image build to ensure docker is
    version 1.11.1+ before adding —label argument

--- cli/main.go
+++ cli/main.go
@@ -78,12 +78,6 @@ func main() {
 	// Parse input flags
 	DefineFlags()
 
-	// seed list: Lists all seed compliant images on (default) local machine
-	if listCmd.Parsed() {
-		DockerList()
-		os.Exit(0)
-	}
-
 	// seed validate: Validate seed.manifest.json.
 	if validateCmd.Parsed() {
 		seedFileName := SeedFileName()
@@ -97,6 +91,15 @@ func main() {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s", err.Error())
 		}
+		os.Exit(0)
+	}
+
+	// Checks if Docker requires sudo access. Prints error message if so.
+	CheckSudo()
+
+	// seed list: Lists all seed compliant images on (default) local machine
+	if listCmd.Parsed() {
+		DockerList()
 		os.Exit(0)
 	}
 
@@ -169,8 +172,11 @@ func DockerBuild(imageName string) {
 
 	// Build Docker image
 	fmt.Fprintf(os.Stderr, "INFO: Building %s\n", imageName)
-	buildCmd := exec.Command("docker", "build", "-t", imageName, jobDirectory,
-		"--label", label)
+	buildArgs := []string{"build", "-t", imageName, jobDirectory}
+	if DockerVersionHasLabel() {
+		buildArgs = append(buildArgs, "--label", label)
+	}
+	buildCmd := exec.Command("docker", buildArgs...)
 
 	// attach stderr pipe
 	errPipe, err := buildCmd.StderrPipe()
@@ -474,11 +480,14 @@ func DockerRun() {
 		}
 	}
 
-	// mount the OUTPUT_DIR (outDir flag)
-	outDir := SetOutputDir(imageName, &seed)
-	if outDir != "" {
-		mountsArgs = append(mountsArgs, "-v")
-		mountsArgs = append(mountsArgs, outDir+":"+outDir)
+	// mount the JOB_OUTPUT_DIR (outDir flag)
+	var outDir string
+	if strings.Contains(seed.Job.Interface.Cmd, "OUTPUT_DIR") {
+		outDir = SetOutputDir(imageName, &seed)
+		if outDir != "" {
+			mountsArgs = append(mountsArgs, "-v")
+			mountsArgs = append(mountsArgs, outDir+":"+outDir)
+		}
 	}
 
 	// Settings
@@ -947,7 +956,7 @@ func SetOutputDir(imageName string, seed *objects.Seed) string {
 	//	auto create a time-stamped subdirectory with the name of the form:
 	//		imagename-iso8601timestamp
 	if outputDir == "" {
-		outputDir = imageName + "-" + time.Now().Format(time.RFC3339)
+		outputDir = "output-" + imageName + "-" + time.Now().Format(time.RFC3339)
 		outputDir = strings.Replace(outputDir, ":", "_", -1)
 	}
 
@@ -1312,4 +1321,83 @@ func GetFullPath(rFile string) string {
 	}
 
 	return rFile
+}
+
+//CheckSudo Checks error for telltale sign seed command should be run as sudo
+func CheckSudo() {
+	cmd := exec.Command("docker", "info")
+
+	// attach stderr pipe
+	errPipe, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"ERROR: Error attaching to version command stderr. %s\n", err.Error())
+	}
+
+	// Run docker build
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Error executing docker version. %s\n",
+			err.Error())
+	}
+
+	slurperr, _ := ioutil.ReadAll(errPipe)
+	er := string(slurperr)
+	if er != "" {
+		if strings.Contains(er, "Cannot connect to the Docker daemon. Is the docker daemon running on this host?") ||
+			strings.Contains(er, "dial unix /var/run/docker.sock: connect: permission deied") {
+			fmt.Fprintf(os.Stderr, "Elevated permissions are required by seed to run Docker. Try running the seed command again as sudo.\n")
+		}
+		os.Exit(1)
+	}
+}
+
+//DockerVersionHasLabel returns if the docker version is greater than 1.11.1
+func DockerVersionHasLabel() bool {
+	cmd := exec.Command("docker", "--version")
+
+	// Attach stdout pipe
+	outPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr,
+			"ERROR: Error attaching to version command stdout. %s\n", err.Error())
+	}
+
+	// Run docker version
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Error executing docker version. %s\n",
+			err.Error())
+	}
+
+	// Print out any std out
+	slurp, _ := ioutil.ReadAll(outPipe)
+	if string(slurp) != "" {
+		// Trim extra text
+		versionStr := strings.TrimPrefix(string(slurp), "Docker version ")
+		versionStr = versionStr[0:strings.Index(versionStr, "-")]
+		version := strings.Split(versionStr, ".")
+
+		// check each part of version. Return false if 1st < 1, 2nd < 11, 3rd < 1
+		if len(version) > 1 {
+			v1, _ := strconv.Atoi(version[0])
+			v2, _ := strconv.Atoi(version[1])
+
+			// check for minimum of 1.11.1
+			if v1 == 1 {
+				if v2 > 11 {
+					return true
+				} else if v2 == 11 && len(version) == 3 {
+					v3, _ := strconv.Atoi(version[2])
+					if v3 >= 1 {
+						return true
+					}
+				}
+			} else if v1 > 1 {
+				return true
+			}
+
+			return false
+		}
+	}
+
+	return false
 }
