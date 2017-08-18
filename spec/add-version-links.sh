2045580db52ad9c4a945b79606d9c080f84ec599
Jonathan Meyer <jon@gisjedi.com>
2017-08-18 10:08:53 -0400

    Updated GitHub pages site to highlight latest tag over master - #82

--- spec/add-version-links.sh
+++ spec/add-version-links.sh
@@ -3,7 +3,7 @@
 # Ensure script directory is CWD
 pushd "${0%/*}"
 
-for VERSION in $(cat ../.versions)
+for VERSION in $(cat ../.versions) master
 do
     echo image:https://img.shields.io/badge/seed-${VERSION}-brightgreen.svg[link="https://ngageoint.github.io/seed/${VERSION}/"] >> index.adoc
 done
--- spec/collect-versioned-docs.sh
+++ spec/collect-versioned-docs.sh
@@ -10,16 +10,21 @@ OUTPUT_DIR=`pwd`
 if [[ "${TRAVIS_TAG}x" != "x" ]]
 then
     sudo mkdir ${TRAVIS_TAG}
-    sudo cp -R seed.* $TRAVIS_TAG/
+    sudo cp -R index.html seed.* $TRAVIS_TAG/
     sudo cp -R ../schema/*.json $TRAVIS_TAG/
 fi
 
 # Grab all available versions to place in gh-pages
 if [[ "${TRAVIS_TAG}x" == "x" ]]
 then
-    # Place snapshot schemas
+    # Place master contents into master directory
+    sudo mkdir -p $OUTPUT_DIR/master/schema
+    sudo cp seed.* master/
+    sudo cp master/seed.hml master/index.html
+    sudo cp ../schema/* $OUTPUT_DIR/master/schema
+
+    # Create folder structure
     sudo mkdir -p $OUTPUT_DIR/schema
-    sudo cp ../schema/* $OUTPUT_DIR/schema
 
     # Place versioned spec and schemas
     for VERSION in $(cat ../../.versions)
@@ -27,11 +32,20 @@ then
         cd $OUTPUT_DIR
         sudo mkdir -p $VERSION/schema
         cd $VERSION
-        sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/index.html
+        sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.html
+        # We want https://ngageoint.github.io/$VERSION/ to also serve up versioned spec... not require /seed.html
+        sudo cp seed.html index.html
         sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.pdf
         cd schema
         sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.manifest.schema.json
         sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.metadata.schema.json
+
+        # We are going to place all versions in the root, the last one will win, which satisfies our goal of latest tag
+        # being the one seen when hitting the GitHub Pages site.
+        cd $OUTPUT_DIR
+        sudo cp -rf $VERSION/* ./
+        sudo rm index.html
+        sudo wget https://github.com/ngageoint/seed/releases/download/${VERSION}/index.html
     done
 fi
 
