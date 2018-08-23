9f5683428c2350237dc5edb4129bed3d168f6eed
Jonathan Meyer <jon@gisjedi.com>
2018-08-23 12:55:58 -0400

    Updated the generate scripts to generate all doc formats

--- spec/generate-outputs.sh
+++ spec/generate-outputs.sh
@@ -16,25 +16,19 @@
 
 pushd $(dirname $0) > /dev/null
 
-# Compile css from sass
+echo Compiling css from sass...
 docker run --rm -v $(pwd):/var/www ${SASS_IMAGE} sh styles/compile-sass.sh
 
-# Generate HTML
+echo Generating HTML...
 docker run -v $(pwd):/documents --rm ${ASCIIDOCTOR_IMAGE} asciidoctor -D /documents/output seed.adoc
 
-# Patch doc for PDF generation
-perl -0777 -i.bak -pe 's/\/\/{pdf\+4}\na/4\+a/g;' -pe 's/\/\/{pdf\+1}\n3\+a/4\+a/g' $(pwd)/sections/standard.adoc
+echo Generating PDF...
+docker run -v $(pwd):/documents --rm ${ASCIIDOCTOR_IMAGE} sh generate-pdf.sh
 
-# Generate PDF
-docker run -v $(pwd):/documents --rm ${ASCIIDOCTOR_IMAGE} asciidoctor-pdf -a pdf-style=styles/pdf-theme.yml -a pdf-fontsdir=styles/fonts/ -D /documents/output seed.adoc
-
-# Generate manpage styled adoc
+echo Generating manpage styled adoc...
 docker run -v $(pwd):/documents --rm ${PYTHON_IMAGE} python /documents/generate-manpage.py
 
-# Generate manpage
+echo Generating manpage...
 docker run -v $(pwd):/documents --rm ${ASCIIDOCTOR_IMAGE} asciidoctor -b manpage -D /documents/output seed.man.adoc
-
-# Replace original doc following PDF generation
-mv $(pwd)/sections/standard.adoc.bak $(pwd)/sections/standard.adoc
 
 popd > /dev/null
