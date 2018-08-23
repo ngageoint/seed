a04b175dfe0f0178744faf9aec1e10446bd31c60
Jonathan Meyer <jon@gisjedi.com>
2018-08-23 13:02:36 -0400

    Improved cross platform sed usage

--- spec/examples/inject-manifests.sh
+++ spec/examples/inject-manifests.sh
@@ -7,12 +7,7 @@ pushd "${0%/*}"
 for DIRECTORY in $(ls -F | grep /)
 do
     SEED_MANIFEST=$(jq -rc . < ${DIRECTORY}/seed.manifest.json | sed 's^\"^\\\\\"^g' | sed 's^\$^\\\\\$^g' | sed 's^/^\\\\/^g')
-    if [[ "`uname`" == "Linux" ]] 
-    then
-        sed -i "s^SEED_MANIFEST^${SEED_MANIFEST}^" ${DIRECTORY}/Dockerfile
-    else
-        sed -i "" -e "s^SEED_MANIFEST^${SEED_MANIFEST}^" ${DIRECTORY}/Dockerfile
-    fi
+    sed -i.bak "s^SEED_MANIFEST^${SEED_MANIFEST}^" ${DIRECTORY}/Dockerfile
 done
 
 popd > /dev/null
