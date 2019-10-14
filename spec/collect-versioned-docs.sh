9d6de49b5ba2a35e8fb5be5c8554f0545fcafbe8
Jonathan Meyer <jon@gisjedi.com>
2019-10-14 14:36:45 -0400

    Made the download URL parameterizable

--- spec/collect-versioned-docs.sh
+++ spec/collect-versioned-docs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 : ${BUILD_TAG:=${TRAVIS_TAG}}
+: ${RELEASE_DOWNLOAD_URL:=https://github.com/ngageoint/seed/releases/download/}
 
 # Ensure script directory is CWD
 OUTPUT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/output
@@ -35,13 +36,13 @@ then
         cd ${OUTPUT_DIR}
         mkdir -p ${VERSION}/schema
         cd ${VERSION}
-        wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.html
+        wget ${RELEASE_DOWNLOAD_URL}${VERSION}/seed.html
         # We want https://ngageoint.github.io/$VERSION/ to also serve up versioned spec... not require /seed.html
         cp seed.html index.html
-        wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.pdf
+        wget ${RELEASE_DOWNLOAD_URL}${VERSION}/seed.pdf
         cd schema
-        wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.manifest.schema.json
-        wget https://github.com/ngageoint/seed/releases/download/${VERSION}/seed.metadata.schema.json
+        wget ${RELEASE_DOWNLOAD_URL}${VERSION}/seed.manifest.schema.json
+        wget ${RELEASE_DOWNLOAD_URL}${VERSION}/seed.metadata.schema.json
 
         # We are going to place all versions in the root, the last one will win, which satisfies our goal of latest tag
         # being the one seen when hitting the GitHub Pages site.
