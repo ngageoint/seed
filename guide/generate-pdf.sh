d4116ef86eb47dce63d663c859e733f110f97876
Matt Anderson <matt.anderson@appliedis.com>
2020-05-27 10:13:30 -0400

    Remove commented out line

--- guide/generate-pdf.sh
+++ guide/generate-pdf.sh
@@ -3,7 +3,6 @@ apk -U add perl
 
 echo Generating PDF...
 asciidoctor-pdf -a pdf-style=styles/pdf-theme.yml -a pdf-fontsdir=styles/fonts/ -D /documents/output index.adoc
-#asciidoctor-pdf -D /documents/output index.adoc
 
 echo Rename output PDF...
 mv /documents/output/index.pdf /documents/output/seed-user-guide.pdf
